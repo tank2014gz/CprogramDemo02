8959a93725796d931b142ebc087ed986c83233d4
tank2014gz <tank2014guangzhou@qq.com>
2019-11-08 20:54:19 +0800

    初始化

---
+++ cmake-build-debug/CMakeFiles/CprogramDemo02.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CprogramDemo02.dir/main.c.o"
+  "CprogramDemo02"
+  "CprogramDemo02.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CprogramDemo02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
